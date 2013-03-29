c8dd6fa497784abe4421c58df5cdfd32e088f9ef
Timo Knapp <knti1012(at)hs-karlsruhe.de>
2013-03-30 00:51:39 +0100

    Datenbank aufsetzen fehler behoben, es müssen noch sql-skripte nachbearbeitet werden!

--- shop/src/main/resources/sql/10_hibernate_sequence.sql
+++ shop/src/main/resources/sql/10_hibernate_sequence.sql
@@ -2,5 +2,5 @@
 -- Jede SQL-Anweisung muss in genau 1 Zeile
 -- Kommentare durch -- am Zeilenanfang
 -- ===============================================================================
-DROP SEQUENCE hibernate_sequence;
+--DROP SEQUENCE hibernate_sequence;
 CREATE SEQUENCE hibernate_sequence START WITH 1000;
--- shop/src/main/resources/sql/50_lieferung.sql
+++ shop/src/main/resources/sql/50_lieferung.sql
@@ -1,7 +1,7 @@
-INSERT INTO lieferung (id,version,lieferant,transport_art,erzeugt,aktualisiert) VALUES (101,0,DHL,STANDARD,02.09.12,02.09.12);
-INSERT INTO lieferung (id,version,lieferant,transport_art,erzeugt,aktualisiert) VALUES (102,0,DHL,STANDARD,02.09.12,02.09.12);
-INSERT INTO lieferung (id,version,lieferant,transport_art,erzeugt,aktualisiert) VALUES (103,0,UPS,EXPRESS,03.09.12,03.09.12);
-INSERT INTO lieferung (id,version,lieferant,transport_art,erzeugt,aktualisiert) VALUES (104,0,DPD,SCHNELL,03.09.12,03.09.12);
+INSERT INTO lieferung (id,version,lieferant,transport_art,erzeugt,aktualisiert) VALUES (101,0,'DHL','STANDARD','02.09.12','02.09.12');
+INSERT INTO lieferung (id,version,lieferant,transport_art,erzeugt,aktualisiert) VALUES (102,0,'DHL','STANDARD','02.09.12','02.09.12');
+INSERT INTO lieferung (id,version,lieferant,transport_art,erzeugt,aktualisiert) VALUES (103,0,'UPS','EXPRESS','03.09.12','03.09.12');
+INSERT INTO lieferung (id,version,lieferant,transport_art,erzeugt,aktualisiert) VALUES (104,0,'DPD','SCHNELL','03.09.12','03.09.12');
 
 
 
