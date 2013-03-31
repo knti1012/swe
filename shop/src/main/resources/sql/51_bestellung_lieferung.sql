1771e5715c54f903774e0a6a28f50c0d5699c051
Timo Knapp <knti1012(at)hs-karlsruhe.de>
2013-03-31 22:04:23 +0200

    Alle daten in Tabellen angepasst, datenbank vollständig aufgesetzt

--- shop/src/main/resources/sql/51_bestellung_lieferung.sql
+++ shop/src/main/resources/sql/51_bestellung_lieferung.sql
@@ -1,7 +1,7 @@
 INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1000,101);
---INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1001,102);
---INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1002,103);
---INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1003,102);
---INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1004,101);
---INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1006,102);
---INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1007,101);
+INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1001,102);
+INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1002,103);
+INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1003,102);
+INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1004,101);
+INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1006,102);
+INSERT INTO bestellung_lieferung (b_fk,l_fk) VALUES (1007,101);
