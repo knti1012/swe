e586316487322138e2ff73531a557e02341eb341
Timo Knapp <knti1012(at)hs-karlsruhe.de>
2013-04-13 11:21:40 +0200

    falsche SQL skripte, beans.xml angepasst

--- shop/sql/01_indexes.sql
+++
@@ -1,9 +0,0 @@
--- ===============================================================================
--- Indexe in den *generierten* Tabellen anlegen
--- ===============================================================================
-CREATE INDEX kunde_adresse_index ON kunde(add_fk);
-CREATE INDEX bestellung_kunde_index ON bestellung(k_fk);
-CREATE INDEX bestpos_bestellung_index ON bestellposition(b_fk);
-CREATE INDEX bestpos_artikel_index ON bestellposition(ar_fk);
-CREATE INDEX be_l_bestellung_index ON bestellung_lieferung(b_fk);
-CREATE INDEX be_l_lieferung_index ON bestellung_lieferung(l_fk);
