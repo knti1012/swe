30e8e8b759f14e380da6aff389d95c924111f316
Vassil Dimov <diva1012@hs-karlsruhe.de>
2013-05-26 14:38:24 +0200

    Layout + kleine andere änderungen

--- shop/src/main/java/de/shop/artikelverwaltung/controller/ArtikelController.java
+++ shop/src/main/java/de/shop/artikelverwaltung/controller/ArtikelController.java
@@ -1,19 +1,32 @@
 package de.shop.artikelverwaltung.controller;
 import static de.shop.util.Constants.JSF_REDIRECT_SUFFIX;
 
+import org.jboss.logging.Logger;
+
+
+
+
 
 import static javax.ejb.TransactionAttributeType.REQUIRED;
 import static javax.ejb.TransactionAttributeType.SUPPORTS;
+import static javax.persistence.PersistenceContextType.EXTENDED;
 import de.shop.util.Client;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.TransactionAttribute;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.Flash;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import de.shop.artikelverwaltung.domain.Artikel;
 import de.shop.artikelverwaltung.service.ArtikelService;
@@ -35,8 +48,12 @@ public class ArtikelController implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -6337596008107442042L;
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
 	private static final String FLASH_ARTIKEL = "artikel";
 	private static final String JSF_VIEW_ARTIKEL = "/artikelverwaltung/viewArtikel";
+	
+	@PersistenceContext(type = EXTENDED)
+	private transient EntityManager em;
 	
 	@Inject
 	private ArtikelService as;
@@ -48,13 +65,33 @@ public class ArtikelController implements Serializable {
 	@Inject
 	private Flash flash;
 	
+	
+	private Artikel artikel;
+	
 	private Long artikelId;
+	
+	private List<Artikel> artikels = Collections.emptyList();
+	
 	
 	
 	private Artikel neuerArtikel; // "Leeres Objekt" fuer Eingabe
-	
-	private Artikel artikel;
 
+	
+//	@Inject
+//	@Push(topic = "test")
+//	private transient Event<String> neuerArtikelEvent;
+
+	@PostConstruct
+	private void postConstruct() {
+		LOGGER.debugf("CDI-faehiges Bean %s wurde erzeugt", this);
+	}
+	
+	@PreDestroy
+	private void preDestroy() {
+		LOGGER.debugf("CDI-faehiges Bean %s wird geloescht", this);
+	}
+
+	
 	@Override
 	public String toString() {
 		return "ArtikelController [artikelId=" + artikelId + "]";
@@ -93,7 +130,10 @@ public class ArtikelController implements Serializable {
 	@TransactionAttribute(REQUIRED)
 	public String createArtikel(){
 		
-			as.createArtikel(neuerArtikel, locale);
+			neuerArtikel = as.createArtikel(neuerArtikel, locale);
+			
+//			neuerArtikelEvent.fire(String.valueOf(neuerArtikel.getId()));
+
 
 			// Aufbereitung fuer viewKunde.xhtml
 			artikelId = neuerArtikel.getId();
